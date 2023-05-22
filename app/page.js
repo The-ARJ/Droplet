747b2a0dc3724e6472c200ad4d765188318583fa
The-ARJ <joshiaayush871@gmail.com>
2023-05-22 19:27:41 +0545

    FAQ implemented

--- app/page.js
+++ app/page.js
@@ -1,11 +1,19 @@
+import ProductSection from "@/components/ProductSection";
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
+import Features from "@/components/Features";
+import FAQ from "@/components/FAQ";
 
 export default function Home() {
   return (
     <main>
       <Header />
       <Hero />
+      <ProductSection />
+      <Features/>
+      <FAQ/>
+      <Footer />
     </main>
   )
 }
---
+++ components/FAQ.jsx
@@ -0,0 +1,228 @@
+"use client";
+import React, { useState } from "react";
+
+const FAQ = () => {
+  const [activeTab, setActiveTab] = useState("");
+
+  const toggleTab = (tab) => {
+    if (activeTab === tab) {
+      setActiveTab("");
+    } else {
+      setActiveTab(tab);
+    }
+  };
+
+  return (
+    <>
+      <section className="bg-white ">
+        <div className="container px-6 py-12 mx-auto">
+          <div className="lg:flex lg:-mx-12">
+            <div className="lg:w-1/3 lg:px-12">
+              <h1 className="text-2xl font-semibold text-center text-gray-800 lg:text-3xl ">
+                Have any Questions?
+              </h1>
+              <div className="mt-8 xl:mt-16">
+                <h1 className="text-xl font-semibold text-gray-800 ">
+                  Table of Content
+                </h1>
+                <div className="mt-4 space-y-4 lg:mt-8">
+                  <a href="#" className="block text-blue-500 hover:underline">
+                    General
+                  </a>
+                  <a href="#" className="block text-gray-500  hover:underline">
+                    Trust & Safety
+                  </a>
+                  <a href="#" className="block text-gray-500  hover:underline">
+                    Services
+                  </a>
+                  <a href="#" className="block text-gray-500  hover:underline">
+                    Billing
+                  </a>
+                  <a href="#" className="block text-gray-500  hover:underline">
+                    Office Cleaning
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="lg:w-2/3 lg:px-12 mt-8 lg:mt-0">
+              <div>
+                <button
+                  className="flex items-center focus:outline-none"
+                  onClick={() => toggleTab("appointment")}
+                >
+                  <svg
+                    className="flex-shrink-0 w-6 h-6 text-blue-500"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M20 12H4"
+                    />
+                  </svg>
+                  <h1 className="mx-4 text-xl text-gray-700 ">
+                    How can I pay for my appointment?
+                  </h1>
+                </button>
+                {activeTab === "appointment" && (
+                  <div className="flex mt-8 md:mx-10">
+                    <span className="border border-blue-500" />
+                    <p className="max-w-3xl px-4 text-gray-500 ">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Magni, eum quae. Harum officiis reprehenderit ex quia
+                      ducimus minima id provident molestias optio nam vel,
+                      quidem iure voluptatem, repellat et ipsa.
+                    </p>
+                  </div>
+                )}
+              </div>
+              <hr className="my-8 border-gray-200 " />
+              <div>
+                <button
+                  className="flex items-center focus:outline-none"
+                  onClick={() => toggleTab("consultation")}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="flex-shrink-0 w-6 h-6 text-blue-500"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M12 4v16m8-8H4"
+                    />
+                  </svg>
+                  <h1 className="mx-4 text-xl text-gray-700 ">
+                    What can I expect at my first consultation?
+                  </h1>
+                </button>
+                {activeTab === "consultation" && (
+                  <div className="mt-4 md:mx-10">
+                    <p className="text-gray-500 ">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Ratione, saepe odit? Officiis quod aliquid natus voluptate
+                      hic ad quam eius deleniti.
+                    </p>
+                  </div>
+                )}
+              </div>
+              <hr className="my-8 border-gray-200 " />
+              <div>
+                <button
+                  className="flex items-center focus:outline-none"
+                  onClick={() => toggleTab("opening_hours")}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="flex-shrink-0 w-6 h-6 text-blue-500"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M12 4v16m8-8H4"
+                    />
+                  </svg>
+                  <h1 className="mx-4 text-xl text-gray-700 ">
+                    What are your opening hours?
+                  </h1>
+                </button>
+                {activeTab === "opening_hours" && (
+                  <div className="mt-4 md:mx-10">
+                    <p className="text-gray-500 ">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Ratione, saepe odit? Officiis quod aliquid natus voluptate
+                      hic ad quam eius deleniti.
+                    </p>
+                  </div>
+                )}
+              </div>
+              <hr className="my-8 border-gray-200 " />
+              <div>
+                <button
+                  className="flex items-center focus:outline-none"
+                  onClick={() => toggleTab("referral")}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="flex-shrink-0 w-6 h-6 text-blue-500"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M12 4v16m8-8H4"
+                    />
+                  </svg>
+                  <h1 className="mx-4 text-xl text-gray-700 ">
+                    Do I need a referral?
+                  </h1>
+                </button>
+                {activeTab === "referral" && (
+                  <div className="mt-4 md:mx-10">
+                    <p className="text-gray-500 ">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Ratione, saepe odit? Officiis quod aliquid natus voluptate
+                      hic ad quam eius deleniti.
+                    </p>
+                  </div>
+                )}
+              </div>
+              <hr className="my-8 border-gray-200 " />
+              <div>
+                <button
+                  className="flex items-center focus:outline-none"
+                  onClick={() => toggleTab("insurance")}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="flex-shrink-0 w-6 h-6 text-blue-500"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M12 4v16m8-8H4"
+                    />
+                  </svg>
+                  <h1 className="mx-4 text-xl text-gray-700 ">
+                    Is the cost of the appointment covered by private health
+                    insurance?
+                  </h1>
+                </button>
+                {activeTab === "insurance" && (
+                  <div className="mt-4 md:mx-10">
+                    <p className="text-gray-500 ">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Ratione, saepe odit? Officiis quod aliquid natus voluptate
+                      hic ad quam eius deleniti.
+                    </p>
+                  </div>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default FAQ;
