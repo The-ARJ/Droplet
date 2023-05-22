508b819f0bbfb2ea510bcda1d0fa2d405d8003c2
The-ARJ <joshiaayush871@gmail.com>
2023-05-22 20:30:40 +0545

    updated

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
+    output: 'export',
+    // Optional: Add a trailing slash to all paths `/about` -> `/about/`
+    // trailingSlash: true,
+    // Optional: Change the output directory `out` -> `dist`
+    // distDir: 'dist',
     reactStrictMode: true,
     basePath: "",
     assetPrefix: "",
-  }
-  
-  module.exports = nextConfig
-  
+  };
+   
+  module.exports = nextConfig;
