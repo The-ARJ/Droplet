fbe41f6f9e0e5ad661e4e52715bffefe29c33225
The-ARJ <joshiaayush871@gmail.com>
2023-05-22 20:02:39 +0545

    faq implemented

--- app/page.js
+++ app/page.js
@@ -4,6 +4,8 @@ import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import Features from "@/components/Features";
 import FAQ from "@/components/FAQ";
+import Boost from "@/components/Boost";
+import Contact from "@/components/Contact";
 
 export default function Home() {
   return (
@@ -11,8 +13,10 @@ export default function Home() {
       <Header />
       <Hero />
       <ProductSection />
+      <Boost/>
       <Features/>
       <FAQ/>
+      <Contact/>
       <Footer />
     </main>
   )
--- components/FAQ.jsx
+++ components/FAQ.jsx
@@ -14,7 +14,7 @@ const FAQ = () => {
 
   return (
     <>
-      <section className="bg-white ">
+      <section className="bg-white max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto ">
         <div className="container px-6 py-12 mx-auto">
           <div className="lg:flex lg:-mx-12">
             <div className="lg:w-1/3 lg:px-12">
