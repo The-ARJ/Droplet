2df94cc14723260f58806b126308a72b2fc98363
The-ARJ <joshiaayush871@gmail.com>
2023-05-23 20:15:19 +0545

    dahboard implemented

---
+++ app/home/page.js
@@ -0,0 +1,12 @@
+import Content from '../../components/content';
+import DashboardLayout from '../../components/dashboard/layout';
+
+export default function HomePage() {
+    return (
+        <>
+            <DashboardLayout>
+                <Content title="Home" />
+            </DashboardLayout></>
+    )
+
+}
