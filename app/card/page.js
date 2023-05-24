1e1f340696afca1fafd1d2d3396d40d8fdc07989
The-ARJ <joshiaayush871@gmail.com>
2023-05-24 15:36:45 +0545

    card page implemented

---
+++ app/card/page.js
@@ -0,0 +1,12 @@
+import CardContent from '@/components/CardContent';
+import DashboardLayout from '../../components/dashboard/layout';
+
+export default function CardPage() {
+    return (
+        <>
+            <DashboardLayout>
+                <CardContent title="Card" />
+            </DashboardLayout></>
+    )
+
+}
