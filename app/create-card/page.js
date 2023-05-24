61b73413fd9136bbe38feff4bd375a537d8f2524
The-ARJ <joshiaayush871@gmail.com>
2023-05-24 17:30:25 +0545

    create card page implemented

---
+++ app/create-card/page.js
@@ -0,0 +1,13 @@
+import CardContent from '@/components/dashboard/CardContent';
+import DashboardLayout from '../../components/dashboard/layout';
+import CreateCard from '@/components/dashboard/CreateCard';
+
+export default function CreateCardPage() {
+    return (
+        <>
+            <DashboardLayout>
+                <CreateCard/>
+            </DashboardLayout></>
+    )
+
+}
