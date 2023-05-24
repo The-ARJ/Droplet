ae14f5f71bc2b083d4bb13ee1e5feec5b0350255
The-ARJ <joshiaayush871@gmail.com>
2023-05-24 15:37:30 +0545

    dash content implemented

---
+++ components/DashContent.jsx
@@ -0,0 +1,19 @@
+"use client";
+import DashCards from "./DashCards";
+import DashInfo from "./DashInfo";
+
+const Content = ({ title }) => {
+  return (
+    <div className="flex flex-wrap justify-center items-center">
+      <div className="w-full ml-6 ">
+        <DashInfo title="Home" />
+        <div className="mt-6 font-bold text-xl text-purple-900">
+          <h>Active Cards</h>
+          <DashCards />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Content;
