ea18924dbbe72cefe99b940903f1c27f5999bb48
The-ARJ <joshiaayush871@gmail.com>
2023-07-04 18:18:39 +0545

    Services and User Context implemented

---
+++ utils/UserServices.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+const baseURL = "http://localhost:3003/users";
+export const imgURL = "http://localhost:3003";
+
+const login = (credentials) => {
+    return axios.post(`${baseURL}/login/user`, credentials);
+};
+
+const register = (userDetails) => {
+    return axios.post(`${baseURL}/`, userDetails);
+};
+
+const updateUser = (userId, updatedUserDetails) => {
+    const config = {
+        headers: {
+            Authorization: `bearer ${window.localStorage.getItem("token")}`,
+        },
+    };
+    return axios.put(`${baseURL}/${userId}`, updatedUserDetails, config);
+};
+
+const auth = {
+    login,
+    register,
+    updateUser
+};
+
+export default auth;
